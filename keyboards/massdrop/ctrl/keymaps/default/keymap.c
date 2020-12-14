b2e6a6cc9af82a08e755359a2ea31792ef5452b1
Kishor Prins <me@68x.ca>
2020-12-14 14:17:19 -0500

    Added raw hid support for default keymap for CTRL

--- keyboards/massdrop/ctrl/keymaps/default/keymap.c
+++ keyboards/massdrop/ctrl/keymaps/default/keymap.c
@@ -1,5 +1,7 @@
 #include QMK_KEYBOARD_H
 
+#include "raw_hid.h"
+
 enum ctrl_keycodes {
     U_T_AUTO = SAFE_RANGE, //USB Extra Port Toggle Auto Detect / Always Active
     U_T_AGCR,              //USB Toggle Automatic GCR control
@@ -126,3 +128,8 @@ bool process_record_user(uint16_t keycode, keyrecord_t *record) {
             return true; //Process all other keycodes normally
     }
 }
+
+void raw_hid_receive(uint8_t *data, uint8_t length) {
+    raw_hid_send(data, length);
+}
+
--- keyboards/massdrop/ctrl/rules.mk
+++ keyboards/massdrop/ctrl/rules.mk
@@ -28,7 +28,7 @@ BLUETOOTH_ENABLE = no       # Enable Bluetooth with the Adafruit EZ-Key HID
 AUDIO_ENABLE = no           # Audio output on port C6
 FAUXCLICKY_ENABLE = no      # Use buzzer to emulate clicky switches
 VIRTSER_ENABLE = no         # USB Serial Driver
-RAW_ENABLE = no             # Raw device
+RAW_ENABLE = yes             # Raw device
 AUTO_SHIFT_ENABLE = no      # Auto Shift
 
 # Custom RGB matrix handling
